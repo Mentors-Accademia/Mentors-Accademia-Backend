bfaa1d56f3d2e62319ec6b9eadb1add4545777f0
unknown <Mentors-Accademia>
2024-01-15 16:58:21 +0500

    first

--- Schema/Role.js
+++ Schema/Role.js
@@ -4,10 +4,12 @@ const { Schema } = mongoose
 const roleSchema = new Schema({
     role: {
         type: String,
+
         require: true
     },
     status: {
         type: String,
+        
         default: "Y"
     }
 })
